168437c8788c6696a18763d4ce9c94e7151df032
juanluispaulanotorres <juanluis.paulano@bosonit.com>
2022-04-28 09:43:31 +0200

    Realizado el ejercicio '01 Display Hide'

---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,32 @@
+import { Component, Renderer2, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+
+export class PrincipalComponent {
+
+  @ViewChild("boton") boton!: ElementRef;
+  @ViewChild("imagen") imagen!: ElementRef;
+
+  constructor(private renderer: Renderer2) { }
+
+
+  toggle() {
+    let btn = this.boton.nativeElement;
+    let img = this.imagen.nativeElement;
+
+    if (btn['innerText'] == "Enseñar") {
+      this.renderer.removeClass(img, "hidden");
+      this.renderer.addClass(img, "shown");
+      btn['innerHTML'] = "Esconder";
+
+    } else {
+      this.renderer.removeClass(img, "shown");
+      this.renderer.addClass(img, "hidden");
+      btn['innerHTML'] = "Enseñar";
+    }
+  }
+}
