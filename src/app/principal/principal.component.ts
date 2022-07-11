b307af73e2bf67f1270558663f3ccab73ddf2b12
juanluispaulanotorres <juanluis.paulano@bosonit.com>
2022-07-11 10:44:55 +0200

    Ejercicio corregido

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -7,25 +7,20 @@ import { Component, Renderer2, ViewChild, ElementRef } from '@angular/core';
 })
 
 export class PrincipalComponent {
-
-  @ViewChild("boton") boton!: ElementRef;
-  @ViewChild("imagen") imagen!: ElementRef;
+  btnText = 'Ocultar';
+  show = true;
 
   constructor(private renderer: Renderer2) { }
 
-  toggle() {
-    let btn = this.boton.nativeElement;
-    let img = this.imagen.nativeElement;
+  displayHide() {
 
-    if (btn['innerText'] == "Enseñar") {
-      this.renderer.removeClass(img, "hidden");
-      this.renderer.addClass(img, "shown");
-      btn['innerHTML'] = "Esconder";
+    this.show = !this.show;
 
+    if (!this.show) {
+      this.btnText = 'Mostrar';
     } else {
-      this.renderer.removeClass(img, "shown");
-      this.renderer.addClass(img, "hidden");
-      btn['innerHTML'] = "Enseñar";
+      this.btnText = 'Ocultar';
     }
+
   }
 }
